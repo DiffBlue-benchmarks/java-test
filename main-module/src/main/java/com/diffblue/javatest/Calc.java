e4dc281153144972bca7e128b3fe2287bbe7f282
Robert (Jamie) Munro <jamie@diffblue.com>
2019-04-08 13:41:03 +0100

    Minimal tests cases to illustrate lack of coverage

--- main-module/src/main/java/com/diffblue/javatest/Calc.java
+++ main-module/src/main/java/com/diffblue/javatest/Calc.java
@@ -36,4 +36,89 @@ public class Calc {
 
     return Integer.parseInt(str);
   }
+
+  /*
+   * As above, but restricted to addition
+   *
+   * Still no coverage
+   */
+  public static int calcAdditionOnly(String str)
+  {
+    int index;
+
+    index = str.lastIndexOf('+');
+    if (index != -1) {
+      return calc(str.substring(0,index)) + calc(str.substring(index + 1));
+    }
+
+    return Integer.parseInt(str);
+  }
+
+  /*
+   * As above, but only takes 0 or 1 '+'
+   *
+   * Still no coverage
+   */
+  public static int calcOneLevel(String str)
+  {
+    int index;
+
+    index = str.lastIndexOf('+');
+    if (index != -1) {
+      return Integer.parseInt(str.substring(0,index)) + Integer.parseInt(str.substring(index + 1));
+    }
+
+    return Integer.parseInt(str);
+  }
+
+  /*
+   * As above, but only return the LHS of the sum, don't add
+   *
+   * Still no coverage
+   */
+  public static int calcLhs(String str)
+  {
+    int index;
+
+    index = str.lastIndexOf('+');
+    if (index != -1) {
+      return Integer.parseInt(str.substring(0,index));
+    }
+
+    return Integer.parseInt(str);
+  }
+
+  /*
+   * As above, but only return the LHS of the sum, don't add
+   *
+   * This does get coverage, both trivial and with a +. This implies the issue is with substr(0, nondet), but substr(nondet) is fine.
+   */
+  public static int calcRhs(String str)
+  {
+    int index;
+
+    index = str.lastIndexOf('+');
+    if (index != -1) {
+      return Integer.parseInt(str.substring(index + 1));
+    }
+
+    return Integer.parseInt(str);
+  }
+
+  /*
+   * Just concatenate strings, don't convert to int
+   *
+   * This works - both substrs seem fine, with and without a second parameter.
+   */
+  public static String calcStringOut(String str)
+  {
+    int index;
+
+    index = str.lastIndexOf('+');
+    if (index != -1) {
+      return str.substring(0,index) + str.substring(index + 1);
+    }
+
+    return str;
+  }
 }
