69e39b9b3f21c0b376ab163812aee4a7d719e7e2
Robert (Jamie) Munro <jamie@diffblue.com>
2019-04-08 13:40:41 +0100

    Add comment

--- main-module/src/main/java/com/diffblue/javatest/Calc.java
+++ main-module/src/main/java/com/diffblue/javatest/Calc.java
@@ -5,6 +5,11 @@ public class Calc {
     return a / b;
   }
 
+  /*
+   * Takes a maths question as a string like "1+2*3" and returns the sum as an int, e.g. 7
+   *
+   * As of 2019-04-08, no coverage is generated, not even trivial "1" => 1
+   */
   static int calc(String str)
   {
     int index;
